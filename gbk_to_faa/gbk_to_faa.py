7449b096bf996a7c520f59b5cb1e1991dab85bc3
Timothy R. Fallon <tfallon@ucsd.edu>
2023-07-15 21:11:24 -0700

    gbk_to_faa update

--- gbk_to_faa/gbk_to_faa.py
+++ gbk_to_faa/gbk_to_faa.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from Bio import SeqIO
 from Bio import GenBank
 import argparse
@@ -21,10 +23,16 @@ for seq_record in SeqIO.parse(input_handle, "genbank") :
     for seq_feature in seq_record.features :
         if seq_feature.type=="CDS" :
             assert len(seq_feature.qualifiers['translation'])==1
-            output_handle.write(">%s from %s\n%s\n" % (
-                   seq_feature.qualifiers['locus_tag'][0],
-                   seq_record.name,
-                   seq_feature.qualifiers['translation'][0]))
+            if 'label' in seq_feature.qualifiers.keys():
+                output_handle.write(">%s from %s\n%s\n" % (
+                       seq_feature.qualifiers['label'][0],
+                       seq_record.name,
+                       seq_feature.qualifiers['translation'][0]))
+            elif 'locus_tag' in seq_feature.qualifiers.keys():
+                output_handle.write(">%s from %s\n%s\n" % (
+                       seq_feature.qualifiers['locus_tag'][0],
+                       seq_record.name,
+                       seq_feature.qualifiers['translation'][0]))
 
 output_handle.close()
 input_handle.close()
